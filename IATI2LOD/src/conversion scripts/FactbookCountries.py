9c63eb2251a6b38310de6b1d857ad3b174722b68
Kasper Brandt <kasperbrandt87@gmail.com>
2013-05-16 16:07:17 +0200

    Updated CIA Factbook mappings.

--- IATI2LOD/src/conversion scripts/FactbookCountries.py
+++ IATI2LOD/src/conversion scripts/FactbookCountries.py
@@ -1,5 +1,5 @@
 ## By Kasper Brandt
-## Last updated on 13-05-2013
+## Last updated on 16-05-2013
 
 from SPARQLWrapper import SPARQLWrapper, JSON
 from rdflib import RDF, RDFS, Literal, URIRef, Namespace, OWL
@@ -9,7 +9,7 @@ import os, sys
 
 # Settings
 turtle_folder = "/media/Acer/School/IATI-data/mappings/"
-country_codelist = "/media/Acer/School/IATI2LOD/IATI2LOD/xml/codelists/Country.xml"
+country_codelist = "/media/Acer/School/IATI-data/xml/codelists/Country.xml"
 
 if not os.path.isdir(turtle_folder):
     os.makedirs(turtle_folder)
@@ -31,19 +31,37 @@ found_countries = 0
 not_found = []
 
 for country in root.findall('Country'):
+    found = False
+    
     total_countries += 1
     code = country.find('code').text.lower()
     name = country.find('name').text
     
     cia_code = "." + code
     
+    print "Looking for code " + code + "..."
+    
+    # Municipalities of The Netherlands:
+    if cia_code == ".bq" or cia_code == ".cw" or cia_code == ".sx":
+        cia_code = ".nl"
+        
+    # Municipalities of France:
+    elif cia_code == ".bl" or cia_code == ".mf":
+        cia_code = ".fr"
+        
+    # United Kingdom exception:
+    elif cia_code == ".gb":
+        cia_code = ".uk"
+    
     sparql = SPARQLWrapper("http://wifo5-03.informatik.uni-mannheim.de/factbook/sparql")
     
     sparql.setQuery("""
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        SELECT ?country
-        WHERE { ?country <http://wifo5-04.informatik.uni-mannheim.de/factbook/ns#internetcountrycode> "%s" }
-        """ % (cia_code))
+        SELECT ?country ?code
+        WHERE { ?country rdf:type <http://wifo5-04.informatik.uni-mannheim.de/factbook/ns#Country> .
+        ?country <http://wifo5-04.informatik.uni-mannheim.de/factbook/ns#internetcountrycode> ?code . }
+        """)
     
     sparql.setReturnFormat(JSON)
     results = sparql.query().convert()
@@ -51,47 +69,49 @@ for country in root.findall('Country'):
     link = ""
 
     for result in results["results"]["bindings"]:
-        link = result["country"]["value"]
-        found_countries += 1
-        break
+        if cia_code in result["code"]["value"]:
+            link = result["country"]["value"]
+            
+            print "Found code " + code + ", link: " + str(link)
+            
+            countries.add((Iati['codelist/Country/' + code],
+                           OWL.sameAs,
+                           URIRef(link)))
+            
+            found_countries += 1
+            found = True
+            break
     
-    print str(code) + ": " + str(link)
-    
-    if not link == "":
-        countries.add((Iati['codelist/Country/' + code],
-                       OWL.sameAs,
-                       URIRef(link)))
-    
-    if link == "":
+    if not found:
         not_found.append((name, code))
         
 # Manually adding countries that could not be found
-manual_add = [("ax", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Finland"),
-              ("bq", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Netherlands"),
-              ("cw", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Netherlands"),
-              ("fi", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Finland"),
-              ("fr", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/France"),
-              ("gf", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/France"),
-              ("gp", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/France"),
-              ("mq", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/France"),
-              ("mp", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/United_States"),
-              ("ps", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Israel"),
-              ("re", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/France"),
-              ("ru", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Russia"),
-              ("bl", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/France"),
-              ("sh", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Saint_Helena"),
-              ("mf", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/France"),
-              ("rs", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Serbia"),
-              ("sx", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Netherlands"),
-              ("ss", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Sudan"),
-              ("tl", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/East_Timor"),
-              ("gb", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/United_Kingdom"),
-              ("um", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/United_States")]
-
-for country in manual_add:
-    countries.add((Iati['codelist/Country/' + country[0]],
-                   OWL.sameAs,
-                   URIRef(country[1])))
+#manual_add = [("ax", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Finland"),
+#              ("bq", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Netherlands"),
+#              ("cw", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Netherlands"),
+#              ("fi", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Finland"),
+#              ("fr", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/France"),
+#              ("gf", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/France"),
+#              ("gp", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/France"),
+#              ("mq", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/France"),
+#              ("mp", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/United_States"),
+#              ("ps", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Israel"),
+#              ("re", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/France"),
+#              ("ru", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Russia"),
+#              ("bl", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/France"),
+#              ("sh", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Saint_Helena"),
+#              ("mf", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/France"),
+#              ("rs", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Serbia"),
+#              ("sx", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Netherlands"),
+#              ("ss", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/Sudan"),
+#              ("tl", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/East_Timor"),
+#              ("gb", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/United_Kingdom"),
+#              ("um", "http://wifo5-04.informatik.uni-mannheim.de/factbook/resource/United_States")]
+#
+#for country in manual_add:
+#    countries.add((Iati['codelist/Country/' + country[0]],
+#                   OWL.sameAs,
+#                   URIRef(country[1])))
 
 # Adding mappings to file
 print
@@ -108,7 +128,7 @@ print "Automatically found: " + str(found_countries)
 print
 print "Could not automatically find:"
 for country in not_found:
-    if (country[1] == 'cr') or (country[1] == 'so'):
-        print country[1] + ": " + country[0] + ", could not be found at all..."
-    else:
-        print country[1] + ": " + country[0]
+#    if (country[1] == 'cr') or (country[1] == 'so'):
+#        print country[1] + ": " + country[0] + ", could not be found at all..."
+#    else:
+    print country[1] + ": " + country[0]
